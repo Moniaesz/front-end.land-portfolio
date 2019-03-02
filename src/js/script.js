ac5b7dbb0dd94843bdc23e46bb394d2cad4aa3ac
Monika <molinskamonika+github@gmail.com>
2019-03-02 18:40:06 +0100

    header & portfolio section: basic styles

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
 
+	const menu = document.querySelector('.fa-bars');
+	const nav = document.querySelector('.header__nav');
+
+	console.log(menu, nav);
+
+// mobile navigation
+	menu.addEventListener('click', function() {
+		nav.classList.toggle('open');
+	});
+
+});
