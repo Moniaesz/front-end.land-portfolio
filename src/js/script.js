9028ddb215f521b2848002ebbd2258524af7b66c
Monika <molinskamonika+github@gmail.com>
2019-03-02 22:53:54 +0100

    footer with Leaflet map

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+// render map with custom marker
+var map = L.map('map').setView([52.409538, 16.931990], 12);
+
+var mapIcon = L.icon({
+	iconUrl: 'assets/logo-map.svg',
+	iconSize: [37, 95]
+})
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+L.marker([52.423820, 16.91996], {icon: mapIcon}).addTo(map);
+
+//dark mode switcher
 	const body = document.getElementsByTagName('body')[0];
 	const bulb = document.querySelector('.bulb');
 	const logo = document.querySelector('.logo');
 	const footerLogo = document.querySelector('.footer__logo');
-	const menu = document.querySelector('.fa-bars');
-	const nav = document.querySelector('.header__nav');
 
-	console.log(body, bulb, logo, menu, nav);
-
-//dark mode switcher
 	bulb.addEventListener('click', function() {
 		body.classList.toggle('dark');
 		if (body.classList.contains('dark')) {
@@ -22,6 +32,9 @@ document.addEventListener("DOMContentLoaded", function() {
 	});
 
 // mobile navigation
+	const menu = document.querySelector('.fa-bars');
+	const nav = document.querySelector('.header__nav');
+
 	menu.addEventListener('click', function() {
 		nav.classList.toggle('open');
 	});
