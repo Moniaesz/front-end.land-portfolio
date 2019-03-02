b6885eee9fadb8c65ef7ea772a9c1fc660073dd9
Monika <molinskamonika+github@gmail.com>
2019-03-02 20:24:43 +0100

    dark mode switcher

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,25 @@
 document.addEventListener("DOMContentLoaded", function() {
 
+	const body = document.getElementsByTagName('body')[0];
+	const bulb = document.querySelector('.bulb');
+	const logo = document.querySelector('.logo');
+	const footerLogo = document.querySelector('.footer__logo');
 	const menu = document.querySelector('.fa-bars');
 	const nav = document.querySelector('.header__nav');
 
-	console.log(menu, nav);
+	console.log(body, bulb, logo, menu, nav);
+
+//dark mode switcher
+	bulb.addEventListener('click', function() {
+		body.classList.toggle('dark');
+		if (body.classList.contains('dark')) {
+			logo.src = 'assets/logo-black.svg';
+			footerLogo.src = 'assets/logo-black.svg';
+		} else {
+			logo.src = 'assets/logo.svg';
+			footerLogo.src = 'assets/logo.svg';
+		}
+	});
 
 // mobile navigation
 	menu.addEventListener('click', function() {
